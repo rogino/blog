a5233550b6e65ab1bc633653af6e53109e0a1f79
Rio Ogino <rogino@users.noreply.github.com>
2024-01-14 14:06:24 +1300

    Change slug to include year

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://rioogino.com/",
+  author: "Rio Ogino",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Rio Ogino",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
